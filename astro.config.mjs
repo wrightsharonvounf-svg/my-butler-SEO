d28b7d1170ac2a13a1e76ccc54547a3047a6b696
Timur_Suhov <timur.suhov@gmail.com>
2026-01-23 20:09:40 +0300

    Fix: site add

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://wrightsharonvounf-svg.github.io/my-butler-SEO/',
+  site: 'https://butler-tim.ru',
   // Убираем интеграцию sitemap, так как у нас есть свой postbuild.js
   // integrations: [sitemap()], 
   
