462d7e0f2b6223278729f212293ed540e63d8c9d
Timur_Suhov <timur.suhov@gmail.com>
2026-01-25 14:42:33 +0300

    Fix: add new config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.butler-tim.ru',
+  site: 'https://butler-tim.ru',
   integrations: [sitemap()],
   output: 'static',
   
