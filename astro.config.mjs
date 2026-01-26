302aea7111ee15daa14a9a44b6383bbe61acbdec
Timur_Suhov <timur.suhov@gmail.com>
2026-01-26 09:24:51 +0300

    fix to yandex

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://butler-tim.ru',
+  site: 'https://www.butler-tim.ru',
   integrations: [sitemap()],
   output: 'static',
   
