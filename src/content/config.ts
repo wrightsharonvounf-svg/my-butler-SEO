761ef159d38545dce1733e4276d615bb3607c0da
Timur_Suhov <timur.suhov@gmail.com>
2026-03-06 22:08:42 +0300

    new file new config

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,28 @@
 import { defineCollection, z } from 'astro:content';
 
 const posts = defineCollection({
+  type: 'content',
+
   schema: z.object({
     title: z.string(),
+
     description: z.string(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
+
+    pubDate: z.date(),
+
+    updatedDate: z.date().optional(),
+
+    author: z.string().optional(),
+
     tags: z.array(z.string()).optional(),
+
+    cluster: z.enum([
+      "taxes",
+      "profit",
+      "districts",
+      "investments"
+    ]),
+
     faq: z.array(
       z.object({
         question: z.string(),
